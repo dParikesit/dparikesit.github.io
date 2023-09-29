74ceb29ce333fdabfd247f0bb8edcfb6ec0ec4b7
Dimas Shidqi Parikesit <dsparikesit@gmail.com>
2023-09-29 08:12:35 +0700

    Setup blog

--- src/config.ts
+++ src/config.ts
@@ -1,16 +1,16 @@
 import type { Site, SocialObjects } from "./types";
 
 export const SITE: Site = {
-  website: "https://astro-paper.pages.dev/",
-  author: "Sat Naing",
-  desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
-  title: "AstroPaper",
+  website: "https://dparikesit.github.io/",
+  author: "dParikesit",
+  desc: "Dimas Shidqi Parikesit personal blog. Everything is my own opinion.",
+  title: "dParikesit",
   ogImage: "astropaper-og.jpg",
   lightAndDarkMode: true,
-  postPerPage: 3,
+  postPerPage: 5,
 };
 
-export const LOCALE = ["en-EN"]; // set to [] to use the environment default
+export const LOCALE = ["en-EN", "id-ID"]; // set to [] to use the environment default
 
 export const LOGO_IMAGE = {
   enable: false,
@@ -22,38 +22,38 @@ export const LOGO_IMAGE = {
 export const SOCIALS: SocialObjects = [
   {
     name: "Github",
-    href: "https://github.com/satnaing/astro-paper",
-    linkTitle: ` ${SITE.title} on Github`,
+    href: "https://github.com/dParikesit",
+    linkTitle: `Checkout ${SITE.title} on Github`,
     active: true,
+  },
+  {
+    name: "LinkedIn",
+    href: "https://www.linkedin.com/in/dimas-parikesit/",
+    linkTitle: `Have job opportunities? ${SITE.title} on LinkedIn`,
+    active: true,
+  },
+  {
+    name: "Mail",
+    href: "mailto:dsparikesit@gmail.com",
+    linkTitle: `Send an email to ${SITE.title}`,
+    active: true,
+  },
+  {
+    name: "Twitter",
+    href: "https://twitter.com/dimashidqi",
+    linkTitle: `Follow ${SITE.title} on Twitter`,
+    active: true,
+  },
+  {
+    name: "Instagram",
+    href: "https://www.instagram.com/dimashidqi/",
+    linkTitle: `${SITE.title} on Instagram`,
+    active: false,
   },
   {
     name: "Facebook",
     href: "https://github.com/satnaing/astro-paper",
     linkTitle: `${SITE.title} on Facebook`,
-    active: true,
-  },
-  {
-    name: "Instagram",
-    href: "https://github.com/satnaing/astro-paper",
-    linkTitle: `${SITE.title} on Instagram`,
-    active: true,
-  },
-  {
-    name: "LinkedIn",
-    href: "https://github.com/satnaing/astro-paper",
-    linkTitle: `${SITE.title} on LinkedIn`,
-    active: true,
-  },
-  {
-    name: "Mail",
-    href: "mailto:yourmail@gmail.com",
-    linkTitle: `Send an email to ${SITE.title}`,
-    active: false,
-  },
-  {
-    name: "Twitter",
-    href: "https://github.com/satnaing/astro-paper",
-    linkTitle: `${SITE.title} on Twitter`,
     active: false,
   },
   {
