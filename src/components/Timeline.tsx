9c1d860ecc86201928dc8558cae41b25e5451fa2
Dimas Shidqi Parikesit <dsparikesit@gmail.com>
2024-07-01 16:40:44 +0700

    feat: timeline

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -5,8 +5,8 @@ export default function Timeline() {
     <ol class="border-gray-200 dark:border-gray-700 relative mt-2 border-s">
       {NEWS.map(data => (
         <li class="mb-1 ms-4">
-          <div class="bg-gray-200 border-white dark:border-gray-900 dark:bg-gray-700 absolute -start-1.5 mt-1.5 h-3 w-3 rounded-full border"></div>
-          <time class="font-normal leading-none">
+          <div class="absolute -start-1.5 mt-1.5 h-3 w-3 rounded-full border"></div>
+          <time class="text-sm font-normal leading-none">
             {data.date} - {data.title}
           </time>
         </li>
